9fa14c186671352ae075974cc93bc44893442985
shieber28 <shieber@vt.edu>
2026-09-23 14:04:44 -0400

    Improve output formatting in about_me.py

--- Practicals/W4/about_me.py
+++ Practicals/W4/about_me.py
@@ -1,1 +1,10 @@
 !/usr/bin/env python.3
+
+#I named multiple different strings and then printed them out together using the print() function. However, when I originially printed them out they weren't seperated into new lines so I learned about the newline function which seperates each line, so I added that to my code.
+
+name = "My name : Sylvia"
+color = "My favorite color : Blue"
+activity = "My favorite acitvity : Playing sports"
+animal = "My favorite animal : Penguin" 
+
+print(name, color, activity, animal, sep='\n')
